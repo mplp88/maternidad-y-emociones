33356c0d8c55b0047bed52eec934fbb2949971e5
Martín Ponce <martinp88@gmail.com>
2025-08-13 01:36:41 -0300

    cambio de pageDescription

--- src/stores/pageData.js
+++ src/stores/pageData.js
@@ -3,9 +3,7 @@ import { defineStore } from 'pinia'
 
 export const usePageDataStore = defineStore('pageData', () => {
   const pageTitle = ref('Maternidad y emociones')
-  const pageDescription = ref(
-    'Mamá y Doula diplomada en psicología puerinatal. Te acompaño con consejos y contención emocional para atravesar momentos difíciles y encontrar tu mejor versión.',
-  )
+  const pageDescription = ref('Mamá y Doula diplomada en psicología puerinatal.')
   const name = ref('Caterina Frias')
 
   return { pageTitle, pageDescription, name }
