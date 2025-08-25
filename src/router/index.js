3e0c278486289e743099e8e9d752b4c44ddd45b2
Martín Ponce <martinp88@gmail.com>
2025-08-25 01:44:04 -0300

    fix scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/',
